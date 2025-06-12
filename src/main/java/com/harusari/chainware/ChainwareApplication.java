9e579fdfbe4dfcb9fd321c0c54d43ac1a3b75e6a
memory-h <gusdud9124@gmail.com>
2025-06-12 10:49:35 +0900

    프로젝트 초기 설정

---
+++ src/main/java/com/harusari/chainware/ChainwareApplication.java
@@ -0,0 +1,13 @@
+package com.harusari.chainware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChainwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChainwareApplication.class, args);
+	}
+
+}
