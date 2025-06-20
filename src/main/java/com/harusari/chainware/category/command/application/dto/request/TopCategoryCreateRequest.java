1a0410a231a51b99cd6e9cd7f466e863a735117e
jihye25 <wisdom95725@gmail.com>
2025-06-20 17:51:04 +0900

    feat: 제품 카테고리 기본 CRUD 구현

---
+++ src/main/java/com/harusari/chainware/category/command/application/dto/request/TopCategoryCreateRequest.java
@@ -0,0 +1,10 @@
+package com.harusari.chainware.category.command.application.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+
+@Builder
+public record TopCategoryCreateRequest(
+        @NotBlank(message = "상위 카테고리명은 필수입니다.") String topCategoryName
+) {
+}
