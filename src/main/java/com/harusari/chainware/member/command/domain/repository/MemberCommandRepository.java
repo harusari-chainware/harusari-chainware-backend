9c017613b7211f31d8cc4af730507b958def35e3
memory-h <gusdud9124@gmail.com>
2025-06-24 15:44:12 +0900

    feat: 회원 정보 수정 기능 구현 (#54)

--- src/main/java/com/harusari/chainware/member/command/domain/repository/MemberCommandRepository.java
+++ src/main/java/com/harusari/chainware/member/command/domain/repository/MemberCommandRepository.java
@@ -2,10 +2,14 @@ package com.harusari.chainware.member.command.domain.repository;
 
 import com.harusari.chainware.member.command.domain.aggregate.Member;
 
+import java.util.Optional;
+
 public interface MemberCommandRepository {
 
     boolean existsByEmail(String email);
 
     Member save(Member member);
+
+    Optional<Member> findMemberByMemberId(Long memberId);
 
 }
