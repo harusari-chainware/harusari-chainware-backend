0cbd0161d1e4719434fb48f4ac8cc4a12f3ffc90
jihye25 <wisdom95725@gmail.com>
2025-06-25 00:44:27 +0900

    feat: 제품 카테고리 조회 기능 추가 및 개선 (#69)

--- src/main/java/com/harusari/chainware/category/query/dto/response/CategoryDetailResponse.java
+++ src/main/java/com/harusari/chainware/category/query/dto/response/CategoryDetailResponse.java
@@ -1,17 +1,23 @@
 package com.harusari.chainware.category.query.dto.response;
 
 import com.harusari.chainware.product.query.dto.response.ProductDto;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
 public class CategoryDetailResponse {
     private Long categoryId;
     private String categoryName;
     private Long topCategoryId;
     private String topCategoryName;
-    private List<ProductDto> products;
+    private List<ProductDto> products = new ArrayList<>();
+
+    public CategoryDetailResponse(Long categoryId, String categoryName, Long topCategoryId, String topCategoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.topCategoryId = topCategoryId;
+        this.topCategoryName = topCategoryName;
+    }
 }
--- src/main/java/com/harusari/chainware/category/query/dto/response/CategoryWithProductsResponse.java
+++ src/main/java/com/harusari/chainware/category/query/dto/response/CategoryWithProductsResponse.java
@@ -1,24 +1,22 @@
 package com.harusari.chainware.category.query.dto.response;
 
 import com.harusari.chainware.product.query.dto.response.ProductDto;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-@AllArgsConstructor
-@Builder
 public class CategoryWithProductsResponse {
     private Long categoryId;
     private String categoryName;
     private Long topCategoryId;
-    private List<ProductDto> products;
+    private List<ProductDto> products = new ArrayList<>();
 
     public CategoryWithProductsResponse(Long categoryId, String categoryName, Long topCategoryId) {
         this.categoryId = categoryId;
         this.categoryName = categoryName;
         this.topCategoryId = topCategoryId;
+        this.products = new ArrayList<>();
     }
 }
