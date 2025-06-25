8d7c4241fe8cdbb0d0eb1ea709e32ac435868539
memory-h <gusdud9124@gmail.com>
2025-06-25 10:06:46 +0900

    refactor: MapStruct 불필요한 옵션 제거 및 디렉토리명을 mapper에서 mapstruct으로 변경 (#72)

--- src/main/java/com/harusari/chainware/franchise/common/mapstruct/FranchiseMapStruct.java
+++ src/main/java/com/harusari/chainware/franchise/common/mapstruct/FranchiseMapStruct.java
@@ -1,14 +1,10 @@
-package com.harusari.chainware.franchise.common.mapper;
+package com.harusari.chainware.franchise.common.mapstruct;
 
 import com.harusari.chainware.franchise.command.domain.aggregate.Franchise;
 import com.harusari.chainware.member.command.application.dto.request.franchise.FranchiseCreateRequest;
 import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
 
-@Mapper(
-        unmappedSourcePolicy =  ReportingPolicy.WARN,
-        unmappedTargetPolicy = ReportingPolicy.WARN
-)
+@Mapper
 public interface FranchiseMapStruct {
 
     Franchise toFranchise(FranchiseCreateRequest franchiseCreateRequest, Long memberId);
