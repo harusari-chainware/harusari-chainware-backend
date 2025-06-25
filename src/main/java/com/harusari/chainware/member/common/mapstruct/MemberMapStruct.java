03eccea68e563fd9302f2c41986f97ae3e0135a8
memory-h <gusdud9124@gmail.com>
2025-06-25 12:40:47 +0900

    refactor: MapStruct 매핑 시 unmapped 경고 무시하도록 설정 변경 (#79)

--- src/main/java/com/harusari/chainware/member/common/mapstruct/MemberMapStruct.java
+++ src/main/java/com/harusari/chainware/member/common/mapstruct/MemberMapStruct.java
@@ -3,8 +3,12 @@ package com.harusari.chainware.member.common.mapstruct;
 import com.harusari.chainware.member.command.application.dto.request.MemberCreateRequest;
 import com.harusari.chainware.member.command.domain.aggregate.Member;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper
+@Mapper(
+        unmappedSourcePolicy =  ReportingPolicy.IGNORE,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 public interface MemberMapStruct {
 
     Member toMember(MemberCreateRequest memberCreateRequest);
--- src/main/java/com/harusari/chainware/vendor/common/mapstruct/VendorMapStruct.java
+++ src/main/java/com/harusari/chainware/vendor/common/mapstruct/VendorMapStruct.java
@@ -3,8 +3,12 @@ package com.harusari.chainware.vendor.common.mapstruct;
 import com.harusari.chainware.member.command.application.dto.request.vendor.VendorCreateRequest;
 import com.harusari.chainware.vendor.command.domain.aggregate.Vendor;
 import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
 
-@Mapper
+@Mapper(
+        unmappedSourcePolicy =  ReportingPolicy.IGNORE,
+        unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
 public interface VendorMapStruct {
 
     Vendor toVendor(VendorCreateRequest vendorCreateRequest, Long memberId);
