3575ca8608b40efffffab5c8ea85462653cc3640
memory-h <gusdud9124@gmail.com>
2025-06-26 16:59:49 +0900

    docs: SecurityPolicy에 가맹점 정보 수정 주석 추가 (#81)

--- src/main/java/com/harusari/chainware/config/security/SecurityPolicy.java
+++ src/main/java/com/harusari/chainware/config/security/SecurityPolicy.java
@@ -65,7 +65,7 @@ public enum SecurityPolicy {
     PRODUCT_DETAIL_GET("/api/v1/products/{productId}", GET, ROLE_BASED, List.of(GENERAL_MANAGER, SENIOR_MANAGER, WAREHOUSE_MANAGER, FRANCHISE_MANAGER)), // 제품 상세 정보 조회
 
     /* Franchise */
-    FRANCHISE_PUT("/api/v1/franchise/{franchiseId}", PUT, ROLE_BASED, List.of(GENERAL_MANAGER, SENIOR_MANAGER)),
+    FRANCHISE_PUT("/api/v1/franchise/{franchiseId}", PUT, ROLE_BASED, List.of(GENERAL_MANAGER, SENIOR_MANAGER)), // 가맹점 정보 수정
 
     /* Vendor */
 
