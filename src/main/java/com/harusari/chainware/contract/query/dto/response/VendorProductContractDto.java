5db4309790d589cb37749191e90c66efcaf78dff
jihye25 <wisdom95725@gmail.com>
2025-06-27 11:59:51 +0900

    feat: 거래처별 제품 조회 기능 및 페이징 처리 추가

--- src/main/java/com/harusari/chainware/contract/query/dto/response/VendorProductContractDto.java
+++ src/main/java/com/harusari/chainware/contract/query/dto/response/VendorProductContractDto.java
@@ -1,5 +1,6 @@
 package com.harusari.chainware.contract.query.dto.response;
 
+import com.harusari.chainware.contract.command.domain.aggregate.ContractStatus;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,29 +10,40 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 public class VendorProductContractDto {
-        private Long contractId;
+    private Long contractId;
+    private String vendorName;
+    private String productName;
+    private Integer basePrice;
     private Integer contractPrice;
     private Integer minOrderQty;
     private Integer leadTime;
+    private ContractStatus contractStatus;
     private LocalDate contractStartDate;
     private LocalDate contractEndDate;
-    private String contractStatus;
-    private LocalDateTime createdAt;
-    private LocalDateTime modifiedAt;
 
-    private Long vendorId;
-
-    private Long productId;
-    private String productName;
-    private Integer basePrice;
-    private String unitQuantity;
-    private String unitSpec;
-    private String storeType;
-
-    private Long topCategoryId;
-    private String topCategoryName;
-
-    private Long categoryId;
-    private String categoryName;
+//    private Long contractId;
+//    private Integer contractPrice;
+//    private Integer minOrderQty;
+//    private Integer leadTime;
+//    private LocalDate contractStartDate;
+//    private LocalDate contractEndDate;
+//    private String contractStatus;
+//    private LocalDateTime createdAt;
+//    private LocalDateTime modifiedAt;
+//
+//    private Long vendorId;
+//
+//    private Long productId;
+//    private String productName;
+//    private Integer basePrice;
+//    private String unitQuantity;
+//    private String unitSpec;
+//    private String storeType;
+//
+//    private Long topCategoryId;
+//    private String topCategoryName;
+//
+//    private Long categoryId;
+//    private String categoryName;
 
 }
