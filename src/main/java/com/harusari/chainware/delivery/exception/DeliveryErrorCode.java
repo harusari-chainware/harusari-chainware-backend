950f271e5fb8f0f84f72783f788adfbd551c246f
z00m-1n <jmlee_1230@ewha.ac.kr>
2025-06-30 17:56:13 +0900

    refactor: 배송 에러코드 수정  (#114)

--- src/main/java/com/harusari/chainware/delivery/exception/DeliveryErrorCode.java
+++ src/main/java/com/harusari/chainware/delivery/exception/DeliveryErrorCode.java
@@ -19,10 +19,10 @@ public enum DeliveryErrorCode {
     PRODUCT_INVENTORY_NOT_FOUND_FOR_DELIVERY("10004", "배송할 제품의 재고 정보를 찾을 수 없습니다.", HttpStatus.BAD_REQUEST),
     INSUFFICIENT_INVENTORY_FOR_DELIVERY("10005", "배송할 제품의 수량이 충분하지 않습니다.", HttpStatus.BAD_REQUEST),
     DELIVERY_STATUS_NOT_IN_TRANSIT("10006", "배송 완료 처리는 'IN_TRANSIT' 상태에서만 가능합니다.", HttpStatus.BAD_REQUEST),
-    PRODUCT_NOT_FOUND_IN_ORDER("10006", "주문에 포함되지 않은 제품입니다.", HttpStatus.BAD_REQUEST),
-    INVALID_DELIVERY_QUANTITY("10006", "배송 불가능한 수량입니다.", HttpStatus.BAD_REQUEST),
-    TAKE_BACK_DETAIL_NOT_FOUND("10006", "존재하는 반품이 아닙니다.", HttpStatus.BAD_REQUEST),
-    INVALID_DELIVERY_DETAIL_ID("10006", "존재하지 않는ㄴ 배송 상세입니다.", HttpStatus.BAD_REQUEST);
+    PRODUCT_NOT_FOUND_IN_ORDER("10007", "주문에 포함되지 않은 제품입니다.", HttpStatus.BAD_REQUEST),
+    INVALID_DELIVERY_QUANTITY("10008", "배송 불가능한 수량입니다.", HttpStatus.BAD_REQUEST),
+    TAKE_BACK_DETAIL_NOT_FOUND("10009", "존재하는 반품이 아닙니다.", HttpStatus.BAD_REQUEST),
+    INVALID_DELIVERY_DETAIL_ID("10010", "존재하지 않는 배송 상세입니다.", HttpStatus.BAD_REQUEST);
 
     private final String errorCode;
     private final String errorMessage;
