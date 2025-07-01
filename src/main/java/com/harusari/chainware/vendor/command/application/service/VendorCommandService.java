5460ca3fa1be39d16e9e5f21d806f4904ba6f661
memory-h <gusdud9124@gmail.com>
2025-07-01 11:36:22 +0900

    refactor: 창고 주소를 우편번호, 도로명 주소, 상세 주소로 세분화 (#127)

--- src/main/java/com/harusari/chainware/vendor/command/application/service/VendorCommandService.java
+++ src/main/java/com/harusari/chainware/vendor/command/application/service/VendorCommandService.java
@@ -1,16 +1,14 @@
 package com.harusari.chainware.vendor.command.application.service;
 
 import com.harusari.chainware.member.command.application.dto.request.vendor.MemberWithVendorRequest;
-import com.harusari.chainware.vendor.command.application.dto.VendorCreateRequestDto;
 import com.harusari.chainware.vendor.command.application.dto.VendorStatusChangeRequestDto;
 import com.harusari.chainware.vendor.command.application.dto.VendorUpdateRequestDto;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface VendorCommandService {
 
-    Long createVendor(VendorCreateRequestDto requestDto);
+    void createVendorWithAgreement(Long memberId, MemberWithVendorRequest memberWithVendorRequest, MultipartFile agreementFile);
     void updateVendor(Long vendorId, VendorUpdateRequestDto dto);
     void changeVendorStatus(Long vendorId, VendorStatusChangeRequestDto dto);
-    void createVendorWithAgreement(Long memberId, MemberWithVendorRequest memberWithVendorRequest, MultipartFile agreementFile);
 
 }
