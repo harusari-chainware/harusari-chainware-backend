170f8b926463bf3f92ef6502ffc1823fc708a483
jihye25 <wisdom95725@gmail.com>
2025-07-07 17:17:41 +0900

    feat: 카테고리 프론트 화면 기능 수정 및 UI 개선

--- src/main/java/com/harusari/chainware/category/query/dto/response/TopCategoryDto.java
+++ src/main/java/com/harusari/chainware/category/query/dto/response/TopCategoryDto.java
@@ -4,13 +4,17 @@ import com.harusari.chainware.category.command.domain.aggregate.TopCategory;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 public class TopCategoryDto {
     private Long topCategoryId;
     private String topCategoryName;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
     public static TopCategoryDto fromEntity(TopCategory entity) {
-        return new TopCategoryDto(entity.getTopCategoryId(), entity.getTopCategoryName());
+        return new TopCategoryDto(entity.getTopCategoryId(), entity.getTopCategoryName(), entity.getCreatedAt(), entity.getModifiedAt());
     }
 }
