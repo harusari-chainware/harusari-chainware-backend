0decb544220952caf07eb222123441ab6c39fde9
jang9465 <jangwh9466@nate.com>
2025-07-10 14:24:49 +0900

    refactor : 목록 리스트 검색을 위한 코드 수정

--- src/main/java/com/harusari/chainware/requisition/query/dto/request/RequisitionSearchCondition.java
+++ src/main/java/com/harusari/chainware/requisition/query/dto/request/RequisitionSearchCondition.java
@@ -4,11 +4,20 @@ import com.harusari.chainware.requisition.command.domain.aggregate.RequisitionSt
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 public class RequisitionSearchCondition {
 
+    private String drafterName;
+    private String approverName;
     private RequisitionStatus status;
+    private String vendorName;
+
+    private LocalDate createdFrom;
+    private LocalDate createdTo;
+
     private int page = 0;
     private int size = 10;
 
