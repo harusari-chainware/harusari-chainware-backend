12e2f99ad28063f7c5e7d5e2a65fd5371724b06f
jihye25 <wisdom95725@gmail.com>
2025-07-12 17:28:33 +0900

    feat: 거래처 별 제품 계약 프론트 화면 기능 구현 및 코드 개선, 추가 제품 마스터, 카테고리 코드 개선 (#176)

--- src/main/java/com/harusari/chainware/category/query/mapper/CategoryQueryMapper.java
+++ src/main/java/com/harusari/chainware/category/query/mapper/CategoryQueryMapper.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @Mapper
 public interface CategoryQueryMapper {
-
     List<CategoryWithTopResponse> searchCategoriesWithTopAndProductCount(
             @Param("request") CategorySearchRequest request,
             @Param("offset") int offset,
